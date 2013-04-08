9e9ac8ebaeb6067344b6c3ff221b5fe6091a35e4
Kris McGinnes <kmcginnes@me.com>
2013-04-08 02:22:02 -0500

    Added InferSuspects method

--- src/InspectorSherlock.Tests/LanguageSpecs.cs
+++ src/InspectorSherlock.Tests/LanguageSpecs.cs
@@ -6,11 +6,23 @@ namespace InspectorSherlock.Tests
     public class LanguageSpecs
     {
         [Fact]
-        public void SherlockShouldBuildCase()
+        public void SherlockShouldBuildCaseResultingInCaseFile()
         {
             var result = Sherlock.BuildCase(1, 2);
 
+            result.Should().NotBeNull();
             result.Should().BeOfType<CaseFile>();
+        }
+
+        [Fact]
+        public void CaseFileShouldInferSuspectsResultingInHypothesis()
+        {
+            var caseFile = new CaseFile();
+
+            var result = caseFile.InferSuspects();
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType<Hypothesis>();
         }
     }
 }
--- src/InspectorSherlock/CaseFile.cs
+++ src/InspectorSherlock/CaseFile.cs
@@ -2,5 +2,13 @@
 {
     public class CaseFile
     {
+        public Hypothesis InferSuspects()
+        {
+            return new Hypothesis();
+        }
+    }
+
+    public class Hypothesis
+    {
     }
 }
